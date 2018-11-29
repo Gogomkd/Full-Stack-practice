4079d4d83e797ff1b6c4e8dc4aa6e48638465352
Gogomkd <goran_i18@yahoo.com>
2018-11-29 12:01:00 +0100

    Introduction to Object Oriented Programming - Examples of objects

---
+++ Frontend/JavaScript/Object-OrientedProgramming/myscript.js
@@ -0,0 +1,80 @@
+// var stadium = {
+//     name : "Old Traford",
+//     seats: 68000,
+//     pitch: "Grass",
+//     facility: true,
+//     gymTypes: ['Fitnes', 'Joga', 'Pool'],
+//     tickets: 35000,
+//     checkEmpty: function(){
+//         return this.seats - this.tickets;
+//     },
+    
+// };
+
+
+// stadium.name = "Delle Alpi";
+// stadium.seats = 45000;
+
+// var player = {
+//     name: "Ronaldo",
+//     lastName: "Junior",
+//     gender: "Male",
+//     team: "Juventus",
+//     age: 33,
+//     bestSeason: function(){
+//         console.log(this.name+" " + this.lastName + " Has his best season when he was " + this.age + " and playing for " + this.team);
+//     }
+// };
+
+
+// var igrac = player;
+
+
+// var team = new Object();
+
+// team.name = "Manchester United";
+// team.location = "England";
+// team.players = 22;
+// team.games = team.wins + team.draw + team.loss;
+// team.draw = 2;
+// team.wins = 3;
+// team.loss = 1;
+// team.points = (team.wins * 3) + team.draw;
+// team.score = 1;
+// team.wins = function(){
+//     this.games * 3;
+// }
+
+// function premierLeague (team){
+//     var matches = 38 - team.games;
+//     var scoreboard = team.points;
+//     console.log("The games played until now is " + matches + " with " + scoreboard + " points");
+// }
+
+// premierLeague(team);
+
+// function Team(name, location){
+//     this.name = name;
+//     this.location = location;
+//     this.color = function(){
+//         console.log(this.name + " is located in " + this.location);
+//     }
+// }
+
+// var manchester = new Team("Manchester", "England");
+// var juventus = new Team("Juventus", "Italy");
+// var realMadrid = new Team("Real Madrid", "Spain");
+
+// function counter (Team) {
+//     brojac = 0;
+//     Team = Tim;
+//     for (var i = Tim; i <= Tim; i++){
+//         brojac += i;
+//     }
+//     console.log("Postojat " + brojac + " timovi");
+// }
+
+// counter(Team);
+
+
+
