9c83328c709a7db5bdf09e26fb94b223ecc5c488
Gogomkd <goran_i18@yahoo.com>
2018-12-25 19:39:18 +0100

    MusicApp building from scratch usig Ajax - Class 17 will be updated with more info

---
+++ Frontend/JavaScript/MusicApp/repositories/artistRepository.js
@@ -0,0 +1,12 @@
+function ArtistRepository() {
+    this.getArtist = async function (artistName) {
+        var result = null;
+        try {
+            var response = await fetch("http://ws.audioscrobbler.com/2.0/?method=artist.getInfo&artist="+artistName+"&api_key=9a32f4fc4d9d7d4157ef376b08a7fa80&format=json");
+            result = await response.json();
+            return new Artist(result.artist);
+        }   catch (error) {
+            return result;
+        }
+    }
+}
