6d692d3e693277c1de3f49f1959fe0df63bfa85b
Gogomkd <goran_i18@yahoo.com>
2019-03-22 19:05:44 +0100

    Landing page - post route to submit email

--- Backend/SalesLeadApp/myapp/controllers/landing.js
+++ Backend/SalesLeadApp/myapp/controllers/landing.js
@@ -1,5 +1,8 @@
-
-
 exports.get_landing = function(req, res, next) {
-  res.render('landing', { title: 'Express' });
-};
+  res.render('landing', { title: 'Express' }); 
+}; // landing page
+
+exports.submit_lead = function(req, res, next) {
+ console.log("Lead Email: ", req.body.lead_email)
+ res.redirect('/');
+}; // submit lead return input value
--- Backend/SalesLeadApp/myapp/routes/index.js
+++ Backend/SalesLeadApp/myapp/routes/index.js
@@ -4,5 +4,5 @@ var router = express.Router();
 let landing  = require('../controllers/landing')
 /* GET home page. */
 router.get('/',  landing.get_landing)
-
+router.post('/', landing.submit_lead)
 module.exports = router;
