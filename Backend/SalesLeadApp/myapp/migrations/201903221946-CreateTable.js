9a54ae608bbfbcc166a727a1f7954366d6b83a0b
Gogomkd <goran_i18@yahoo.com>
2019-03-22 20:29:47 +0100

    Latest version

---
+++ Backend/SalesLeadApp/myapp/migrations/201903221946-CreateTable.js
@@ -0,0 +1,32 @@
+'use strict' ;
+module.exports = {
+   up: (queryInterface, Sequelize) => {
+       return queryInterface.create('Leads', {
+           id: {
+               allowNull: false,
+               primaryKey: true,
+               type: Sequelize.UUID,
+               defaultValue: Sequelize.UUIDV4,
+           },
+           createdAt: {
+               allowNull: false,
+               type: Sequelize.DATE
+           },
+           updatedAt: {
+               allowNull: false,
+               type: Sequelize.DATE
+           },
+            deletedAt: {
+               allowNull: false,
+               type: Sequelize.DATE
+           },
+           email: {
+               allowNull: false,
+               type: Sequelize.STRING
+           },
+       })
+   }
+   down: (queryInterface, Sequelize) => {
+       return queryInterface.dropTable('Leads')
+   }
+};
