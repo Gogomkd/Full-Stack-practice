bac9e4905f523ca757a2a531678191afabb39cc0
Gogomkd <goran_i18@yahoo.com>
2019-03-22 18:54:44 +0100

    landing page getting started, and first form

--- Backend/SalesLeadApp/myapp/routes/index.js
+++ Backend/SalesLeadApp/myapp/routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-let index = require('../controllers/index')
+let landing  = require('../controllers/landing')
 /* GET home page. */
-router.get('/',  index.index)
+router.get('/',  landing.get_landing)
 
 module.exports = router;
