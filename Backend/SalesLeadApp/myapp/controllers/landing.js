dc8473b407ff5d5e3444f8a845f755f10d6fcf2b
Gogomkd <goran_i18@yahoo.com>
2019-03-29 00:56:30 +0100

    Added new route to display lead ditalis

--- Backend/SalesLeadApp/myapp/controllers/landing.js
+++ Backend/SalesLeadApp/myapp/controllers/landing.js
@@ -15,6 +15,16 @@ exports.submit_lead = function(req, res, next) {
 exports.show_leads = function(req, res, next) {
   return models.Lead.findAll().then(leads => {
     res.render('landing', { title: 'Express', leads: leads });
-  })
+  });
+
+};
+  exports.show_lead = function(req, res, next) {
+  return models.Lead.findOne({
+    where : {
+      id : req.params.lead_id
+    }
+  }).then(lead => {
+    res.render('lead', {lead: lead});
+  });
    
 }; // landing page
