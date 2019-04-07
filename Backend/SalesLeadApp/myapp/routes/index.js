f015ff5c51bb80b6490aa2aa412d6763303d24c4
Gogomkd <goran_i18@yahoo.com>
2019-04-07 15:30:29 +0200

    implement passport local stragety
    using email and password fields.

--- Backend/SalesLeadApp/myapp/routes/index.js
+++ Backend/SalesLeadApp/myapp/routes/index.js
@@ -7,6 +7,9 @@ let user = require('../controllers/user')
 
 router.get('/login', user.show_login)
 router.get('/signup', user.show_signup)
+router.post('/login', user.login)
+router.post('/signup', user.signup)
+
 
 router.get('/',  landing.get_landing)
 router.post('/', landing.submit_lead)
