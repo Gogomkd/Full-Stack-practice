3c0cc3fedb3f2e5770bd040264c68b77f220e1e2
Gogomkd <goran_i18@yahoo.com>
2019-04-07 20:11:35 +0200

    Implemented validator with lodash and handling of errors

--- Backend/SalesLeadApp/myapp/controllers/user.js
+++ Backend/SalesLeadApp/myapp/controllers/user.js
@@ -3,7 +3,8 @@ let models = require('../models')
 let bcrypt = require('bcrypt')
 const passport = require('passport')
 const myPassport = require('../passport_setup')(passport)
-
+const {isEmpty} = require('lodash');
+const { validateUser } = require('../validators/signup');
 
 exports.show_login = function(req, res, next) {
     res.render('user/login', {formData: {}, errors: {} })
@@ -13,22 +14,34 @@ exports.show_signup = function(req, res, next){
     res.render('user/signup', { formData: {}, errors: {} })
 }
 
+const rerender_signup = function(errors, req, res, next) {
+	res.render('user/signup', { formData: req.body, errors: errors});
+}
+
 const generateHash = function(password){
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
 }
 
 exports.signup = function(req,res,next){
-     const newUser = models.User.build({
-         email: req.body.email,
-         password: generateHash(req.body.password)
-     });
-     return newUser.save().then(result => {
-         passport.authenticate('local', {
-             successRedirect: "/",
-             failureRedirect: "/signup",
-             failureFlash: true,
-         })(req,res,next);
-     })
+    let errors = {};
+	return validateUser(errors, req).then(errors => {
+		if (!isEmpty(errors)) {
+			rerender_signup(errors, req, res, next);
+		} else {
+            const newUser = models.User.build({
+                    email: req.body.email,
+                    password: generateHash(req.body.password)
+                });
+                return newUser.save().then(result => {
+                    passport.authenticate('local', {
+                        successRedirect: "/",
+                        failureRedirect: "/signup",
+                        failureFlash: true,
+                    })(req,res,next);
+                })
+        }
+  
+    })
 }
 
 exports.login = function(req,res,next){
